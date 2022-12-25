78ab188013e0f7fe36b0658cdf86772292d3f591
EcaterinaS16 <tsconsulting191@gmail.com>
2022-12-25 12:12:30 -0600

    Login  features, page and steps are ready

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -1,6 +1,36 @@
 package steps;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import utils.CommonMethods;
+import utils.ConfigReader;
 
 public class LoginSteps extends CommonMethods {
-}
+    @Given("user navigates to HRMS application")
+    public void user_navigates_to_hrms_application() {
+       openBrowserAndLaunchApplication();
+    }
+
+
+    @When("user enters valid username and valid password")
+    public void user_enters_valid_username_and_valid_password() {
+        sendText(login.usernameTextField, ConfigReader.getPropertyValue("username"));
+
+        WebElement passwordField=driver.findElement(By.id("txtPassword"));
+        sendText(login.passwordTextField, ConfigReader.getPropertyValue("password"));
+    }
+
+    @When("user clicks on login button")
+    public void user_clicks_on_login_button() {
+        click(login.loginButton);
+    }
+
+    @Then("user is successfully logged in")
+    public void user_is_successfully_logged_in() {
+        System.out.println("We logged in successfully");
+
+
+}}
