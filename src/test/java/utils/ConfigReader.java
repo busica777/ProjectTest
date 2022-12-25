ee0a48209e13f6c2d92787a6bc4caa7ecb6429d9
busica777 <gotruck.312@gmail.com>
2022-12-25 01:05:30 -0600

    CommonMethods are ready

--- src/test/java/utils/ConfigReader.java
+++ src/test/java/utils/ConfigReader.java
@@ -1,4 +1,11 @@
 package utils;
 
+import java.util.Properties;
+
 public class ConfigReader {
+    static Properties prop;
+
+    public static String getPropertyValue (String key){
+        return prop.getProperty(key);
+    }
 }
